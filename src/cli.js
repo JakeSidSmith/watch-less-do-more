e356cd6d60f0474e5016e87c5be8a2c125faf0f9
Jake 'Sid' Smith <jake@dabapps.com>
2017-02-24 16:20:26 +0000

    Setup basic yargs & index

--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,31 @@
 #! /usr/bin/env node
+
+'use strict';
+
+(function () {
+
+  var yargs = require('yargs');
+  var watchLessDoMore = require('./index');
+
+  var argv = yargs
+    .usage('watch-less-do-more --input=src/index.less --output=build/index.css')
+    .options({
+      input: {
+        alias: 'i',
+        description: 'Path to input less file',
+        type: 'string'
+      },
+      output: {
+        alias: 'o',
+        description: 'Path to output css file',
+        type: 'string'
+      }
+    })
+    .demandOption(['input', 'output'], 'Please provide both input and output paths')
+    .help()
+    .version()
+    .argv;
+
+  watchLessDoMore({input: argv.input, output: argv.output});
+
+})();
---
+++ src/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+(function () {
+
+  var less = require('less');
+  var _ = require('underscore');
+
+  function watchLessDoMore (options) {
+    console.log(options);
+  }
+
+  module.exports = watchLessDoMore;
+
+})();
