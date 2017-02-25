98f20e7198fd89e6536340203d58a9957a5761e9
Jake 'Sid' Smith <jake@dabapps.com>
2017-02-25 18:42:29 +0000

    Watch file imports

--- src/index.js
+++ src/index.js
@@ -8,9 +8,13 @@
   var _ = require('underscore');
 
   var UTF8 = 'utf8';
-  var CWD = process.cwd();
+  // var CWD = process.cwd();
 
   function watchLessDoMore (options) {
+
+    var parseFileAndWatchImports;
+    var watchers = [];
+
     var lessOptions = {
       filename: path.resolve(options.input),
       compress: options.compress,
@@ -23,8 +27,8 @@
       } : false
     };
 
-    function readFile (input, callback) {
-      fs.readFile(path.join(CWD, input), UTF8, function (error, result) {
+    function readFile (filePath, callback) {
+      fs.readFile(filePath, UTF8, function (error, result) {
         if (error) {
           console.error(error.message);
           process.exit(1);
@@ -34,8 +38,8 @@
       });
     }
 
-    function parseLess (input, callback) {
-      less.render(input, lessOptions, function (error, result) {
+    function parseLess (lessSource, callback) {
+      less.render(lessSource, lessOptions, function (error, result) {
         if (error) {
           console.error(error.message);
           process.exit(1);
@@ -45,11 +49,30 @@
       });
     }
 
-    readFile(options.input, function (result) {
-      parseLess(result, lessOptions, function (output) {
-        console.log(output);
+    function watchFiles (filePaths) {
+      _.each(filePaths, function (filePath) {
+        watchers.push(fs.watch(filePath, UTF8, function (eventType, filename) {
+          console.log(eventType, filename);
+        }));
       });
-    });
+    }
+
+    function destroyWatchers () {
+      _.each(watchers, function (watcher) {
+        watcher.close();
+      });
+    }
+
+    parseFileAndWatchImports = function (inputFilePath) {
+      readFile(inputFilePath, function (result) {
+        parseLess(result, function (output) {
+          destroyWatchers();
+          watchFiles([inputFilePath].concat(output.imports));
+        });
+      });
+    };
+
+    parseFileAndWatchImports(path.resolve(options.input));
   }
 
   module.exports = watchLessDoMore;
